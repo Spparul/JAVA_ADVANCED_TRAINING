f7f5ea277559442db56732e7738cef545ea879ef
Selestine Parul S P <spparul2006@gmail.com>
2026-03-23 05:34:54 +0530

    Leetcode326.java

---
+++ Leetcode326.java
@@ -0,0 +1,8 @@
+class Leetcode326 {
+    public boolean isPowerOfThree(int n) {
+        if(n==1) return true;
+        if(n>0&&1162261467 % n == 0) return true;
+        
+        return false;
+    }
+}
