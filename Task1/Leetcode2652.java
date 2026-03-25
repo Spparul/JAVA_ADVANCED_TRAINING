6bd3b2abf4792cc1db61fbe1e21ce1bb6176c0ab
Selestine Parul S P <spparul2006@gmail.com>
2026-03-25 08:42:44 +0530

    .

--- Task1/Leetcode2652.java
+++ Task1/Leetcode2652.java
@@ -1,6 +1,6 @@
 class Leetcode2652 {
     public int sumOfMultiples(int n) {
-        int sum=0;
+        int sum=0; 
         for(int i=1;i<=n;i++)
         {if(i%3==0||i%5==0||i%7==0) sum+=i;}
         return sum;
